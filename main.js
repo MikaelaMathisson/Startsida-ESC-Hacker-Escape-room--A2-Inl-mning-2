01889c994156be87e83e09259ace359dd9e7eb63
Mikaela Mathisson <mikaela.mathisson@hotmail.se>
2023-11-09 16:07:42 +0100

    changes to filese

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
+const closeBtn = document.getElementsByClassName('closeBtn');
+const menuBtn = document.getElementById('hamburgerMenu');
+const popupContainer = document.getElementById('popup-container');
+const modalBackground = document.querySelector('modalBackgroundHidden');
+
 
 /* Open */
 function openNav() {
-  document.getElementById("myNav").style.display = "block";
-  document.getElementById("myNav").style.margin = "20px 20px";
-}
+    document.getElementById("myNav").style.display = "block";
+    document.getElementById("myNav").style.margin = "20px 20px";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+  
 
-/* Close */
-function closeNav() {
-  document.getElementById("myNav").style.display = "none";
-}
+
+
+menuBtn.addEventListener('click', () => {
+    modalBackground.classList.remove("hidden");
+})
+
+closeBtn.addEventListener('click', () => {
+    modalBackground.classList.add('hidden');
+})
+
+
 
