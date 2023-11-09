629243799c6ac9d37a91a430012ce66e7a46ac07
Mikaela Mathisson <mikaela.mathisson@hotmail.se>
2023-11-09 17:16:57 +0100

    changes

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const closeBtn = document.getElementsByClassName('closeBtn');
 const menuBtn = document.getElementById('hamburgerMenu');
-const popupContainer = document.getElementById('popup-container');
+const popupContainer = document.getElementById('popupContainer');
 const modalBackground = document.querySelector('modalBackgroundHidden');
+
+
 
 
 /* Open */
@@ -15,16 +17,15 @@ function openNav() {
     document.getElementById("myNav").style.display = "none";
   }
   
+  /*Hide navbar and transparent background until click */
+  
 
-
-
+/* Transparent background */ 
 menuBtn.addEventListener('click', () => {
-    modalBackground.classList.remove("hidden");
+    modalBackgroundHidden.classList.remove("hidden");
 })
 
 closeBtn.addEventListener('click', () => {
-    modalBackground.classList.add('hidden');
+    modalBackgroundHidden.classList.add('hidden');
 })
-
-
 
