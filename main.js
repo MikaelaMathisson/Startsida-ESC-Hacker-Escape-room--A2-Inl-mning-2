fc6a3e25d69f2298e2239688e64336b0bae8a7c4
Mikaela Mathisson <mikaela.mathisson@hotmail.se>
2023-11-09 14:29:18 +0100

    changes

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-använd https://codepen.io/jegatheesprana/pen/ZEKmeaq
 
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+  document.getElementById("myNav").style.margin = "20px 20px";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
 
